274f252d74aee7ad7ada414fcdf31f2f7367b8bd
Supriyaksunil <100855324+Supriyaksunil@users.noreply.github.com>
2025-12-17 23:57:45 +0530

    Update app.py

--- foliostack_website/app.py
+++ foliostack_website/app.py
@@ -9,11 +9,11 @@ app = Flask(__name__)
 app.secret_key = "your_secret_key"
 
 db = mysql.connector.connect(
-    host=os.getenv("MYSQLHOST"),
-    user=os.getenv("MYSQLUSER"),
-    password=os.getenv("MYSQLPASSWORD"),
-    database=os.getenv("MYSQLDATABASE"),
-    port=int(os.getenv("MYSQLPORT"))
+    host=os.environ.get("MYSQLHOST"),
+    user=os.environ.get("MYSQLUSER"),
+    password=os.environ.get("MYSQLPASSWORD"),
+    database=os.environ.get("MYSQLDATABASE"),
+    port=int(os.environ.get("MYSQLPORT", 3306))
 )
 
 cursor = db.cursor(dictionary=True)
